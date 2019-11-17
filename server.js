3dd0e25366442cba78fb3d49d479242b0fd9139b
colin <clawrence005@yahoo.com>
2019-11-17 14:04:42 -0600

    added 404 status, and route params work

--- server.js
+++ server.js
@@ -63,29 +63,39 @@ let list2 = [{
   location: "found along rivers and ditches thoughout the Spanish speaking regions of the Americas.",
   photo: "../assets/e4e8ebdc.jpg"
 },];
+
+var express = require("express");
 const app = require("express")();
 const path = require("path");
+var PORT = process.env.PORT || 5000;
 var monsters = require("./data/monstersData.json");
 // Set views directory and views engine as Handlebars using,
-app.set("views", path.join(__dirname, "views"))
-app.set("view engine", "hbs")
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+// serve static files
+app.use(express.static(__dirname + '/public'));
 // Setup a GET route at ‘/’ using,
 app.get("/", (req, res) => {
-
-
   // let monsterList = monster.monsterData;
   console.log("monster", monsters);
+
   res.render("index", { listitem: list, listitem2: list2 });
 });
 
 app.get("/beastiary/:routeName", function (req, res) {
   for (var i = 0; i < list2.length; i++) {
     if (list2[i].routeName === req.params.routeName) {
-      return res.render("beast", list2[i]);
-    } else {
-      res.render("404page");
+      res.render("beast", list2[i]);
+    }
+    else if (list2[i].routeName != req.params.routeName) {
+      res.status(404);
+      res.render('404page');
     }
   }
+});
+app.use(function (req, res) {
+  res.status(404);
+  res.render('404page');
 });
 
 // app.get("beastiary/:routeName", function (req, res) {
@@ -95,5 +105,10 @@ app.get("/beastiary/:routeName", function (req, res) {
 //     }
 //   }
 // })
+
+
 // Finally, setup port for our application using,
-app.listen(5000);
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+  console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.');
+});
