b0c49e764cd26594b4f16e500861e9833a5a4583
colin <clawrence005@yahoo.com>
2019-11-19 00:51:07 -0600

    fixed parsing error, cleaned up code

--- server.js
+++ server.js
@@ -1,83 +1,64 @@
-// var express = require("express");
 
-// var app = express();
-// var PORT = process.env.PORT || 3000;
-// var exphbs = require("express-handlebars");
-
-// var monsters = require("./data/monstersData.json");
-// console.log("monsters", monsters.monsterData[9])
-
-// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-// app.set("view engine", "handlebars");
-
-
-// var express = require('express');
-// var router = express.Router();
-
-// router.get('/', function (req, res) {
-//   res.render('home', { layout: 'default', template: 'home-template' });
-// });
-
-// router.get('/cat', function (req, res) {
-//   res.render("hi cat");
-// })
-
-// app.listen(PORT, function () {
-//   console.log("App listening on PORT " + PORT);
-// });
-let list2 = [{
-  name: "Ghouls",
-  routeName: "ghoul",
-  malicious: true,
-  description: "a",
-  foods: [
-    "cats",
-    "humans",
-    "goats"
-  ],
-  location: "a",
-  photo: "../assets/0d415c72a02ca63254db865c1d0c1f96.gif"
-},
-{
-  name: "Jinn",
-  routeName: "jinn",
-  malicious: true,
-  description: "b",
-  foods: [
-    "cats",
-    "humans",
-    "goats"
-  ],
-  location: "a",
-  "photo": "../assets/12271233430751.56aaa2bdaf27f.png"
-},
-{
-  name: "la Llorona",
-  routeName: "lallorona",
-  description: "A sprectral 'wailing woman' in Spanish she haunts river and ditch banks searching for the children she drowned in madness.",
-  malicious: true,
-  foods: [
-    "children"
-  ],
-  location: "found along rivers and ditches thoughout the Spanish speaking regions of the Americas.",
-  photo: "../assets/e4e8ebdc.jpg"
-},];
+// let list2 = [{
+//   name: "Ghouls",
+//   routeName: "ghoul",
+//   malicious: true,
+//   description: "a",
+//   foods: [
+//     "cats",
+//     "humans",
+//     "goats"
+//   ],
+//   location: "a",
+//   photo: "../assets/0d415c72a02ca63254db865c1d0c1f96.gif"
+// },
+// {
+//   name: "Jinn",
+//   routeName: "jinn",
+//   malicious: true,
+//   description: "b",
+//   foods: [
+//     "cats",
+//     "humans",
+//     "goats"
+//   ],
+//   location: "a",
+//   "photo": "../assets/12271233430751.56aaa2bdaf27f.png"
+// },
+// {
+//   name: "la Llorona",
+//   routeName: "lallorona",
+//   description: "A sprectral 'wailing woman' in Spanish she haunts river and ditch banks searching for the children she drowned in madness.",
+//   malicious: true,
+//   foods: [
+//     "children"
+//   ],
+//   location: "found along rivers and ditches thoughout the Spanish speaking regions of the Americas.",
+//   photo: "../assets/e4e8ebdc.jpg"
+// },];
 
 var express = require("express");
 const app = require("express")();
 const path = require("path");
 var PORT = process.env.PORT || 5000;
 var monsters = require("./data/monstersData.json");
+
 // Set views directory and views engine as Handlebars using,
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 // serve static files
 app.use(express.static(__dirname + '/public'));
 // Setup a GET route at ‘/’ using,
 app.get("/", (req, res) => {
-  console.log("monster", monsters);
+  console.log("monster", monsters[0]);
 
-  res.render("index", { listitem: monsters, listitem2: list2 });
+  res.render("index", {
+    // listitem: monsters,
+    //  listitem2: list2,
+    monsterslist: monsters
+  });
 });
 
 app.get("/mon", function (req, res) {
@@ -85,14 +66,33 @@ app.get("/mon", function (req, res) {
 });
 
 app.get('/data', function (req, res, next) {
-  res.json({ monsters });
+  res.json(monsters);
+
 });
 
 
 app.get("/beastiary/:routeName", function (req, res, next) {
-  for (var i = 0; i < list2.length; i++) {
-    if (list2[i].routeName === req.params.routeName) {
-      res.render("beast", list2[i]);
+
+  for (var i = 0; i < monsters.length; i++) {
+    if (monsters[i].routeName === req.params.routeName) {
+      res.render("beast", monsters[i]);
+    }
+    // else if (list2[i].routeName != req.params.routeName) {
+    //   res.status(404);
+    //   res.render('404page');
+    // }
+    // else {
+    // res.status(404);
+    // res.render('404page');
+    // }
+
+  }
+});
+
+app.get("/monsters/:routeName", function (req, res, next) {
+  for (var i = 0; i < monsters.length; i++) {
+    if (monsters[i].routeName === req.params.routeName) {
+      res.render("beast", monsters[i]);
     }
     // else if (list2[i].routeName != req.params.routeName) {
     //   res.status(404);
@@ -105,6 +105,7 @@ app.get("/beastiary/:routeName", function (req, res, next) {
 
   }
 });
+
 app.use(function (req, res) {
   res.status(404);
   res.render('404page');
