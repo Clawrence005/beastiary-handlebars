08ffb2767b3cb4560a369186f14fc258968b05c5
colin <clawrence005@yahoo.com>
2019-11-19 03:11:19 -0600

    updated version and syntax

--- server.js
+++ server.js
@@ -1,56 +1,37 @@
-
-// let list2 = [{
-//   name: "Ghouls",
-//   routeName: "ghoul",
-//   malicious: true,
-//   description: "a",
-//   foods: [
-//     "cats",
-//     "humans",
-//     "goats"
-//   ],
-//   location: "a",
-//   photo: "../assets/0d415c72a02ca63254db865c1d0c1f96.gif"
-// },
-// {
-//   name: "Jinn",
-//   routeName: "jinn",
-//   malicious: true,
-//   description: "b",
-//   foods: [
-//     "cats",
-//     "humans",
-//     "goats"
-//   ],
-//   location: "a",
-//   "photo": "../assets/12271233430751.56aaa2bdaf27f.png"
-// },
-// {
-//   name: "la Llorona",
-//   routeName: "lallorona",
-//   description: "A sprectral 'wailing woman' in Spanish she haunts river and ditch banks searching for the children she drowned in madness.",
-//   malicious: true,
-//   foods: [
-//     "children"
-//   ],
-//   location: "found along rivers and ditches thoughout the Spanish speaking regions of the Americas.",
-//   photo: "../assets/e4e8ebdc.jpg"
-// },];
-
 var express = require("express");
-const app = require("express")();
-const path = require("path");
-var PORT = process.env.PORT || 5000;
-var monsters = require("./data/monstersData.json");
 
-// Set views directory and views engine as Handlebars using,
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "hbs");
+var PORT = process.env.PORT || 5000;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// serve static files
-app.use(express.static(__dirname + '/public'));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// var express = require("express");
+// const app = require("express")();
+// const path = require("path");
+// var PORT = process.env.PORT || 5000;
+var monsters = require("./data/monstersData.json");
+
+// // Set views directory and views engine as Handlebars using,
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "handlebars");
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// // serve static files
+// app.use(express.static(__dirname + '/public'));
 // Setup a GET route at ‘/’ using,
+
 app.get("/", (req, res) => {
   console.log("monster", monsters[0]);
 
